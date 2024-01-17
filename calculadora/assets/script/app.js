a5934748606097fac91642009073f1f112099188
Pedro Cesar <119058413+PedroCesar-Dev@users.noreply.github.com>
2024-01-17 17:08:50 -0300

    Delete calculadora directory

--- calculadora/assets/script/app.js
+++
@@ -1,8 +0,0 @@
-let valor = document.getElementById('valorEquipamento') 
-let sucata = document.getElementById('valorSucata')
-let vidaUtil = document.getElementById('vidaUtil')  
-
-function depreciacaoItem(){
-    const depreciacao = (valor.value - sucata.value)/vidaUtil.value;
-    document.getElementById('resultado').value = depreciacao + "R$";
-}   
